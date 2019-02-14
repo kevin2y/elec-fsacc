e97f731ae7b78e693f92dd3a5577a2a49dc27d3a
fsyyhua <fsyyhua@hotmail.com>
2019-02-14 22:40:57 +0800

    准备处理录入框

--- src/renderer/components/BankDay.vue
+++ src/renderer/components/BankDay.vue
@@ -1,7 +1,11 @@
 <template>
 <div>
-    <h2>现金日记帐</h2>
-  <el-table :data="tableData3" style="width: 100%;" highlight-current-row :header-cell-style="headCellStyle" :row-style="rowStyle">
+    
+    <el-container style="width: 100%;height: 100%; border: 1px solid #eee">
+
+      <el-main>
+        <h2>现金日记帐</h2>
+        <el-table :data="tableData3" style="width: 100%;" highlight-current-row :header-cell-style="headCellStyle" :row-style="rowStyle">
     <el-table-column prop="date" label="日期" width="150" height="40"></el-table-column>
     <el-table-column prop="BillNo" label="凭证" width="100"></el-table-column>
     <el-table-column prop="Desc" label="摘要" width="100"></el-table-column>
@@ -20,6 +24,24 @@
       </el-table-column>      
     </el-table-column>
   </el-table>
+      </el-main>
+      <el-footer style="height:150px;">
+        <el-form v-enterToNext>
+        <el-row :gutter="24">
+          <el-col :span="6"><div>日期：<el-date-picker v-model="value1" @blur="nextFocus('afterPickDate')" type="date" placeholder="选择日期">
+    </el-date-picker></div></el-col>
+    <input type="hidden"  ></input>
+          <el-col :span="3"><div>凭证：<el-input placeholder="请输入凭证号" ref="afterPickDate" v-model="input2">  </el-input></div></el-col>
+          <el-col :span="12"><div>摘要：<el-input  placeholder="请输入摘要"        v-model="input2">  </el-input></div></el-col>
+          <el-col :span="3"><div>方向：<el-input    placeholder="请输入摘要"        v-model="input2">  </el-input></div> </el-col>
+          </el-row>
+          <el-row :gutter="24">
+            <el-col :span="8" :offset="9"> 金额：<el-input    placeholder="请输入摘要"        v-model="input2">  </el-input></el-col>
+          </el-row>
+        </el-form>
+      </el-footer>
+    </el-container>
+  
   </div>
 </template>
 
@@ -27,6 +49,8 @@
   export default {
     data() {
       return {
+        value1:null,
+        input2:null,
         currentRow:null,
         tableData3: [{
           date: '2016-05-03',
@@ -93,6 +117,19 @@
         },
         handleCurrentChange(val) {
           this.currentRow = val;
+        },
+        nextFocus(refName){
+          console.log("nextFocus..." + refName);
+          let el = this.$refs[refName].$el;
+          let ctl = el.querySelector('input');
+          console.log(ctl);
+          ctl.focus(); //最后一个input，焦点跳转到表单最后控件  
+        },
+        datePick(elday){
+          console.log("datePick...");
+          console.log(elday);          
+          let ctl = elday.querySelector('input');          
+          ctl.focus(); 
         }
     },
     filters: {
